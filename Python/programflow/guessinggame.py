908fe797e78199c205ee53819ff2da4495888cd6
thiagosfluz <thiagosfluz@gmail.com>
2020-03-29 20:55:03 -0300

    new

---
+++ Python/programflow/guessinggame.py
@@ -0,0 +1,45 @@
+answer = 5
+
+print("Please guess number between 1 and 10 : ")
+guess = int(input())
+
+if guess == answer:
+    print("You got it first time")
+
+else:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guesse it")
+    else:
+        print("Sorry , you have not guessed correctly")
+
+
+
+
+
+
+#
+# print("Please guess number between 1 and 10: ")
+# guess = int(input())
+#
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+#
+# else:
+#     print ("You got it first time")
