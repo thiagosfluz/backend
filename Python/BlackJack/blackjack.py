423b241d6a1bcaab4cfa72d64efb3912d92cf4e1
thiagosfluz <thiagosfluz@gmail.com>
2020-04-18 15:42:35 -0300

    new

--- Python/BlackJack/blackjack.py
+++ Python/BlackJack/blackjack.py
@@ -4,7 +4,6 @@ try:
 except ImportError:  #python 2
     import Tkinter as tkinter
 
-mainWindow = tkinter.Tk()
 
 def load_images(card_images):
     suits = ['heart', 'club', 'diamond', 'spade']
@@ -30,24 +29,73 @@ def load_images(card_images):
                 image = tkinter.PhotoImage(file=name)
                 card_images.append((10, image,))
 
+
 def deal_card(frame):
-    #pop the next card off the top of the deck
-    next_card = deck.pop()
-    #add the image to a label and display the label
+    #  pop the next card off the top of the deck
+    next_card = deck.pop(0)
+    # add the image to a label and display the label
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
     # now return the card's face value
     return next_card
+
+def score_hand(hand):
+    # calculate the total score of all cards in the list
+    # Only one ace can have the value 11, and this will be reduced to 1 if the hand would bust
+    score = 0
+    ace = False
+    for next_card in hand:
+        card_value = next_card[0]
+        if card_value == 1 and not ace:
+            ace = True
+            card_value = 11
+        score += card_value
+        # if we would bust, check if there is an ace and subtract 10
+        if score > 21 and ace:
+            score -= 10
+            ace = False
+    return score
+
+
 
 def deal_dealer():
     deal_card(dealer_card_frame)
 
 def deal_player():
-    deal_card(player_card_frame)
+    player_hand.append(deal_card(player_card_frame))
+    player_score = score_hand(player_hand)
+
+    player_score_label.set(player_score)
+    if player_score > 21:
+        result_text.set("Dealer Wins")
+
+
+    # global player_score
+    # global player_ace
+    #
+    # card_value = deal_card(player_card_frame)[0]
+    # if card_value == 1 and not player_ace:
+    #     player_ace = True
+    #     card_value = 11
+    # player_score += card_value
+    # #if we would bust, check if there is an ace and subtract
+    # if player_score > 21 and player_ace:
+    #     player_score -=10
+    #     player_ace = False
+    #     player_score_label.set(player_score)
+    # if player_score > 21:
+    #         result_text.set("Dealer Wins")
+    # print(locals())
+
+
+
+mainWindow = tkinter.Tk()
 
 #Set up the screen and frames for the dealer and player
 
 mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
+mainWindow.configure(background = "green")
+
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
@@ -65,6 +113,7 @@ dealer_card_frame = tkinter.Frame(card_frame, background= 'green')
 dealer_card_frame.grid(row = 0, column =1, sticky = 'ew', rowspan = 2)
 
 player_score_label = tkinter.IntVar()
+
 tkinter.Label(card_frame, text= 'Player', background = "green", fg = "white").grid(row = 2, column = 0)
 tkinter.Label(card_frame, textvariable = player_score_label, background="green", fg="white").grid(row = 3, column = 0)
 
