5e07a821030b928b3a890d28c82c40f019be6841
thiagosfluz <thiagosfluz@gmail.com>
2020-04-19 12:16:14 -0300

    new

--- Python/BlackJack/blackjack.py
+++ Python/BlackJack/blackjack.py
@@ -58,7 +58,24 @@ def score_hand(hand):
 
 
 def deal_dealer():
-    deal_card(dealer_card_frame)
+
+    dealer_score = score_hand(dealer_hand)
+    while 0 < dealer_score < 17:
+        dealer_hand.append(deal_card(dealer_card_frame))
+        dealer_score_label.set(dealer_score)
+        dealer_score = score_hand(dealer_hand)
+
+    player_score = score_hand(player_hand)
+    if player_score > 21:
+        result_text.set("Dealer Wins")
+    elif dealer_score > 21 or dealer_score < player_score:
+        result_text.set("Player Wins!")
+    elif dealer_score > player_score:
+        result_text.set("Dealer Wins!")
+    else:
+        result_text.set("Draw!")
+
+
 
 def deal_player():
     player_hand.append(deal_card(player_card_frame))
@@ -145,5 +162,11 @@ random.shuffle(deck)
 
 dealer_hand = []
 player_hand = []
+deal_player()
+dealer_hand.append(deal_card(dealer_card_frame))
+dealer_score_label.set(score_hand(dealer_hand))
+deal_player()
+
+
 
 mainWindow.mainloop()
